bb7e17a1cc230c2d003036d251f37cf1520ab840
nghinn1012 <21020338@vnu.edu.vn>
2024-08-24 11:56:24 +0700

    done setup notifi with example msg

--- server/notification-service/index.ts
+++ server/notification-service/index.ts
@@ -2,7 +2,7 @@ import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from 'cors';
 import { connectDB } from "./src/db";
-import { NOTIFICATION_PORT } from "./src/config/notification.config";
+import { NOTIFICATION_PORT, USER_SERVICE } from "./src/config/notification.config";
 import notiRouter from "./src/routes/notiRoutes";
 import { publishMessage, subscribeMessage } from "./src/broker/broker";
 const nodemailer = require("nodemailer");
@@ -10,18 +10,17 @@ const nodemailer = require("nodemailer");
 const app = express();
 app.use(cors());
 
-connectDB();
+// connectDB();
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 
 app.use(notiRouter);
 
-
 app.listen(NOTIFICATION_PORT, () => {
   console.log(`Notification-Service running on port ${NOTIFICATION_PORT}`);
 });
 
 subscribeMessage().then(() => {
-  publishMessage("User service", "Hello from Message service");
+  publishMessage(USER_SERVICE, "Hello from Message service");
 });
--- server/notification-service/src/config/notification.config.ts
+++ server/notification-service/src/config/notification.config.ts
@@ -14,3 +14,4 @@ export const NOTIFICATION_SERVICE = process.env.NOTIFICATION_SERVICE as string;
 export const AMQP_PATH = process.env.AMQP_PATH as string;
 export const RPC_QUEUE_NAME = process.env.RPC_QUEUE_NAME as string;
 export const RPC_REQUEST_TIME_OUT = +(process.env.RPC_REQUEST_TIME_OUT || 2000) as number;
+export const USER_SERVICE = process.env.USER_SERVICE as string;
