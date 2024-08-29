b9730b06bb1bdb1690bbe5e11294b6dbb11bee2c
nghinn1012 <21020338@vnu.edu.vn>
2024-08-29 16:12:13 +0700

    chore: update env and config 1

--- server/notification-service/src/config/notification.config.ts
+++ server/notification-service/src/config/notification.config.ts
@@ -9,9 +9,15 @@ export const NODE_MAILER_CONFIG_HOST = process.env.NODE_MAILER_CONFIG_HOST as st
 export const NODE_MAILER_CONFIG_PORT = +(process.env.NODE_MAILER_CONFIG_PORT || 587) as number;
 export const NODE_MAILER_SENDER = process.env.NODE_MAILER_SENDER as string;
 
-export const EXCHANGE_NAME = process.env.EXCHANGE_NAME as string;
+export const USER_SERVICE = process.env.USER_SERVICE as string;
 export const NOTIFICATION_SERVICE = process.env.NOTIFICATION_SERVICE as string;
+export const POST_SERVICE = process.env.POST_SERVICE as string;
+
+export const USER_SERVICE_RPC_QUEUE = process.env.USER_SERVICE_RPC_QUEUE as string;
+export const NOTIFICATION_SERVICE_RPC_QUEUE = process.env.NOTIFICATION_SERVICE_RPC_QUEUE as string;
+export const POST_SERVICE_RPC_QUEUE = process.env.POST_SERVICE_RPC_QUEUE as string;
+
+export const EXCHANGE_NAME = process.env.EXCHANGE_NAME as string;
 export const AMQP_PATH = process.env.AMQP_PATH as string;
 export const RPC_QUEUE_NAME = process.env.RPC_QUEUE_NAME as string;
 export const RPC_REQUEST_TIME_OUT = +(process.env.RPC_REQUEST_TIME_OUT || 2000) as number;
-export const USER_SERVICE = process.env.USER_SERVICE as string;
--- server/user-service/src/config/users.config.ts
+++ server/user-service/src/config/users.config.ts
@@ -12,11 +12,17 @@ export const AC_PRIVATE_KEY = process.env.AC_PRIVATE_KEY as string;
 export const RF_PRIVATE_KEY = process.env.RF_PRIVATE_KEY as string;
 
 export const EXCHANGE_NAME = process.env.EXCHANGE_NAME as string;
-export const USER_SERVICE = process.env.USER_SERVICE as string;
-export const NOTIFICATION_SERVICE = process.env.NOTIFICATION_SERVICE as string;
 export const AMQP_PATH = process.env.AMQP_PATH as string;
 export const RPC_QUEUE_NAME = process.env.RPC_QUEUE_NAME as string;
 export const RPC_REQUEST_TIME_OUT = +(process.env.RPC_REQUEST_TIME_OUT || 2000) as number;
+
+export const USER_SERVICE = process.env.USER_SERVICE as string;
+export const NOTIFICATION_SERVICE = process.env.NOTIFICATION_SERVICE as string;
+export const POST_SERVICE = process.env.POST_SERVICE as string;
+
+export const USER_SERVICE_RPC_QUEUE = process.env.USER_SERVICE_RPC_QUEUE as string;
+export const NOTIFICATION_SERVICE_RPC_QUEUE = process.env.NOTIFICATION_SERVICE_RPC_QUEUE as string;
+export const POST_SERVICE_RPC_QUEUE = process.env.POST_SERVICE_RPC_QUEUE as string;
 
 export const CLOUDINARY_CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME as string;
 export const CLOUDINARY_API_KEY = process.env.CLOUDINARY_API_KEY as string;
