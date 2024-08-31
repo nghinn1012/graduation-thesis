78a39671447a62aad570c8e8c1f93b43d9aa57f5
nghinn1012 <21020338@vnu.edu.vn>
2024-08-31 15:08:52 +0700

    init client with tailwind and daisyui

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,16 @@
-import { FunctionComponent } from "react";
-import "./App.css";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"; // Correct import
-import Register from "./auth/Register";
-import Login from "./auth/Login";
-import Profile from "./pages/Profile";
-import { useAuth } from "./contexts/AuthContext";
-import VerifyEmail from "./auth/Verify";
-import Home from "./auth/Home";
-
-interface AppProps {}
-
-const App: FunctionComponent<AppProps> = () => {
-  const { isAuthenticated } = useAuth();
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/verify" element={<VerifyEmail />} />
-        <Route path="/" element={!isAuthenticated ? <Register /> : <Navigate to="/profile"/>} />
-        <Route path="/login" element={!isAuthenticated ? <Login /> : <Navigate to="/profile"/>} />
-        <Route path="/profile" element={isAuthenticated ? <Profile /> : <Navigate to="/"/>} />
-      </Routes>
-    </BrowserRouter>
-  );
-};
-
-export default App;
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
+import LoginPage from "./pages/auth/login/LoginPage";
+export function App() {
+	return (
+		<div className='flex max-w-6xl mx-auto'>
+			<Routes>
+				<Route path='/' element={<HomePage />} />
+				<Route path='/signup' element={<SignUpPage />} />
+				<Route path='/login' element={<LoginPage />} />
+			</Routes>
+		</div>
+	);
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import {App} from './App'
+import './index.css'
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import { GoogleOAuthProvider } from '@react-oauth/google';
-import "./App.css"
-import { AuthProvider } from './contexts/AuthContext';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <AuthProvider>
-    <GoogleOAuthProvider clientId='703442721289-hup7m1n49sjm9dmd9tn97jqojdum3pol.apps.googleusercontent.com'>
-      <App />
-    </GoogleOAuthProvider>
-    </AuthProvider>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </StrictMode>
+  );
+}
