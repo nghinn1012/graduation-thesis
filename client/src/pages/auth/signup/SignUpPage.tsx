6a45ba86db0d4d2ad8c4dcf115c17f65937a5954
nghinn1012 <21020338@vnu.edu.vn>
2024-09-05 00:32:47 +0700

    i18n for signup and login

--- client/src/pages/auth/signup/SignUpPage.tsx
+++ client/src/pages/auth/signup/SignUpPage.tsx
@@ -1,10 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { userFetcher } from "../../../api/user";
 import toast, { Toaster } from "react-hot-toast";
 import { useGoogleLogin } from "@react-oauth/google";
 import { useAuthContext } from "../../../hooks/useAuthContext";
 import ContentFooter from "../../../components/footer/ContentFooter";
+import * as yup from "yup";
+import { useI18nContext } from "../../../hooks/useI18nContext";
+import { useFormik } from "formik";
 
 const SignUpPage = () => {
   const auth = useAuthContext();
@@ -15,21 +18,52 @@ const SignUpPage = () => {
     confirmPassword: "",
   });
   const navigate = useNavigate();
+  const languageContext = useI18nContext();
+  const lang = languageContext.of(SignUpPage);
 
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    userFetcher
-      .manualRegister(formData)
-      .then(() => {
-        toast.success(
-          "Account created successfully. Please verify your email"
-        );
-        setTimeout(() => navigate("/verify", { state: formData }), 2000)}
-      )
-      .catch((error) => {
-        toast.error(error);
-      });
+  const createSignUpSchema = (lang: any) => {
+    return yup.object({
+      name: yup.string().required("require-name"),
+      email: yup
+        .string()
+        .required(lang("require-email"))
+        .email(lang("invalid-email")),
+      password: yup
+        .string()
+        .required(lang("require-password"))
+        .matches(/[a-z]/, lang("at-least-one-lower-case"))
+        .matches(/[A-Z]/, lang("at-least-one-upper-case"))
+        .matches(/[0-9]/, lang("at-least-one-digit"))
+        .matches(/[!@#$%^&*(),.?":{}|<>]/, lang("at-least-one-special"))
+        .matches(/^\S*$/, lang("no-white-space"))
+        .min(8, lang("invalid-length-password")),
+      confirmPassword: yup
+        .string()
+        .oneOf([yup.ref("password")], lang("not-match"))
+        .required(lang("confirm-pass-require")),
+    });
   };
+
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      name: "",
+      password: "",
+      confirmPassword: "",
+    },
+    validationSchema: createSignUpSchema(lang),
+    onSubmit: (values) => {
+      userFetcher
+        .manualRegister(values)
+        .then(() => {
+          toast.success("Account created successfully. Please verify your email");
+          setTimeout(() => navigate("/verify", { state: values }), 2000);
+        })
+        .catch((error) => {
+          toast.error(error);
+        });
+    },
+  });
   const handleSuccess = (tokenResponse: any) => {
     console.log(tokenResponse);
     try {
@@ -53,12 +87,17 @@ const SignUpPage = () => {
   }
   const handleOAuth = useGoogleLogin({
     onSuccess: handleSuccess,
-    onError: () => console.log('Login Failed'),
+    onError: () => console.log("Login Failed"),
   });
 
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
+  useEffect(() => {
+    formik.setFormikState((prev) => ({
+      ...prev,
+      validationSchema: createSignUpSchema(lang),
+    }));
+    formik.validateForm();
+  }, [languageContext.language]);
+
 
   return (
     <div className="min-h-screen">
@@ -79,27 +118,46 @@ const SignUpPage = () => {
               </label>
               <input
                 type="text"
-                className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
-                name="name"
+                className={`w-full p-2 border ${
+                  formik.touched.name && formik.errors.name
+                    ? "border-red-500"
+                    : "border-gray-300"
+                } rounded-md placeholder:font-light placeholder:text-gray-500`}
                 id="name"
                 placeholder="Enter your name"
-                onChange={handleInputChange}
-                value={formData.name}
+                value={formik.values.name}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.name && formik.errors.name ? (
+                <div className="text-red-500 text-sm">
+                  {formik.errors.name}
+                </div>
+              ) : null}
             <div className="w-1/2">
               <label htmlFor="email" className="mb-2 text-md block">
                 Email
               </label>
               <input
                 type="email"
-                className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
+                className={`w-full p-2 border ${
+                  formik.touched.email && formik.errors.email
+                    ? "border-red-500"
+                    : "border-gray-300"
+                } rounded-md placeholder:font-light placeholder:text-gray-500`}
                 name="email"
                 id="email"
                 placeholder="Enter your email"
-                onChange={handleInputChange}
-                value={formData.email}
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
+             {formik.touched.email && formik.errors.email ? (
+                <div className="text-red-500 text-sm">
+                  {formik.errors.email}
+                </div>
+              ) : null}
             </div>
           </div>
 
@@ -111,12 +169,22 @@ const SignUpPage = () => {
               type="password"
               name="password"
               id="password"
-              className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
-              placeholder="Create a password"
-              onChange={handleInputChange}
-              value={formData.password}
+              className={`w-full p-2 border ${
+                formik.touched.password && formik.errors.password
+                  ? "border-red-500"
+                  : "border-gray-300"
+              } rounded-md placeholder:font-light placeholder:text-gray-500`}
+              placeholder="Enter your password"
+              value={formik.values.password}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
           </div>
+          {formik.touched.password && formik.errors.password ? (
+                <div className="text-red-500 text-sm">
+                  {formik.errors.password}
+                </div>
+              ) : null}
           <div className="py-4">
             <label htmlFor="confirm-password" className="mb-2 text-md">
               Confirm Password
@@ -125,13 +193,23 @@ const SignUpPage = () => {
               type="password"
               name="confirmPassword"
               id="confirm-password"
-              className="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500"
-              placeholder="Confirm your password"
-              onChange={handleInputChange}
-              value={formData.confirmPassword}
+              placeholder="Enter your confirm password"
+              className={`w-full p-2 border ${
+                formik.touched.confirmPassword && formik.errors.confirmPassword
+                  ? "border-red-500"
+                  : "border-gray-300"
+              } rounded-md placeholder:font-light placeholder:text-gray-500`}
+              value={formik.values.confirmPassword}
+              onChange={formik.handleChange}
+              onBlur={formik.handleBlur}
             />
           </div>
-          <button className="w-full bg-black text-white p-2 rounded-lg mb-6 hover:bg-white hover:text-black hover:border hover:border-gray-300" type="submit" onClick={handleSubmit}>
+          {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
+                <div className="text-red-500 text-sm">
+                  {formik.errors.confirmPassword}
+                </div>
+              ) : null}
+          <button className="w-full bg-black text-white p-2 rounded-lg mb-6 hover:bg-white hover:text-black hover:border hover:border-gray-300" type="submit">
             Sign up
           </button>
           <button
